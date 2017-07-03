d3d37db639c222a2c1a30781510845b8e232b4bd
dianacamacho <dcam1901@gmail.com>
2017-07-02 22:01:19 -0500

    popup html and js to add up budget amounts using chrome api for storage

---
+++ popup.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  chrome.storage.sync.get('total', function(budget) {
+    $('#total').text(budget.total);
+  })
+
+  $('#spend-amount').click(function() {
+    chrome.storage.sync.get('total', function(budget) {
+      var newTotal = 0;
+      if (budget.total) {
+        newTotal += parseInt(budget.total);
+      }
+
+      var amount = $('#amount').val();
+
+      if (amount) {
+        newTotal += parseInt(amount);
+      }
+
+      chrome.storage.sync.set({
+        'total': newTotal
+      })
+
+      $('#total').text(newTotal);
+      $('#amount').val('');
+    })
+  })
+})
